a1b15f4dfb37600a167e3198b1a1a35ff7029ab4
LUCIFER <palacios.cesar.2avesp@gmail.com>
2023-03-08 09:22:44 -0600

    001_CESAR
    CREE REPO
    CREE PROGRA

---
+++ 23_03_08_001_HOLA_MUNDO_/23_03_08_001_HOLA_MUNDO_.cpp
@@ -0,0 +1,12 @@
+// 23_03_08_001_HOLA_MUNDO_.cpp :
+// CESAR DANIEL PALACIOS BEAS  
+// ESTE ES MI PRIMER PROGRAMA 
+// Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
